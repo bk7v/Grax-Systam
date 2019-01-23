7350e2103e3207748b1c4f6a5f575de93b838cb3
bk7v <46638986+bk7v@users.noreply.github.com>
2019-01-23 19:27:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -179,8 +179,7 @@ lient.on('message', message => {
 
 
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+
 client.on('message',async message => {
   if(message.author.bot) return;
 var prefix = "^^"
