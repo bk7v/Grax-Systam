9985604458d00d6e3f297f50a4e679040e6e7e98
bk7v <46638986+bk7v@users.noreply.github.com>
2019-01-26 12:14:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -319,107 +319,32 @@ client.on('message', async msg => { //Toxic Codes
 
 
 
-
- client.on('message', message => {
-         if (!message.channel.guild) return;
- 
-  let room = message.content.split(" ").slice(1);
-  let findroom = message.guild.channels.find('name', `${room}`)
-  if(message.content.startsWith(prefix + "^^setimge")) {
-      if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
-      if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
-      if(!room) return message.channel.send('Please Type The Channel Name')
-      if(!findroom) return message.channel.send('Cant Find This Channel')
-      let embed = new Discord.RichEmbed()
-      .setTitle('**Done The MediaOnly Code Has Been Setup**')
-      .addField('Channel:', `${room}`)
-      .addField('Requested By', `${message.author}`)
-      .setThumbnail(message.author.avatarURL)
-      .setFooter(`${client.user.username}`)
-      message.channel.sendEmbed(embed)
-      pics[message.guild.id] = {
-      channel: room,
-      onoff: 'On'
-      },
-      fs.writeFile("./pics.json", JSON.stringify(pics), (err) => {
-      if (err) console.error(err)
-     
-      })
-    }})
-       
-client.on('message', message => {
- 
-  if(message.content.startsWith(prefix + "toggleimge")) {
-          if (!message.channel.guild) return;
- 
-      if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
-      if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
-      if(!pics[message.guild.id]) pics[message.guild.id] = {
-        onoff: 'Off'
-      }
-        if(pics[message.guild.id].onoff === 'Off') return [message.channel.send(`**The MediaCode Is __𝐎𝐍__ !**`), pics[message.guild.id].onoff = 'On']
-        if(pics[message.guild.id].onoff === 'On') return [message.channel.send(`**The MediaCode Is __𝐎𝐅𝐅__ !**`), pics[message.guild.id].onoff = 'Off']
-        fs.writeFile("./pics.json", JSON.stringify(pics), (err) => {
-          if (err) console.error(err)
-         
-          })
-        }
-       
-      })
-     
-             client.on('message', message => {
-                       if (!message.channel.guild) return;
+client.on('message', ( message ) => {
   if(message.author.bot) return;
- 
-        if(!pics[message.guild.id]) pics[message.guild.id] = {
-        onoff: 'Off'
-      }
-        if(pics[message.guild.id].onoff === 'Off') return;
- 
-  if(message.channel.name !== `${pics[message.guild.id].channel}`) return;
- 
-   let types = [
+
+  if(message.channel.id !== '538604594205556757') return;
+
+  let types = [
     'jpg',
     'jpeg',
-    'png'
-  ]
-   if (message.attachments.size <= 0) {
+    'png',
+    'gif',
+    'mp4',
+    'avi',
+    'mkv',
+    'mpeg'
+  ]//Toxic Codes
+
+  if (message.attachments.size <= 0) {
     message.delete();
-    message.channel.send(`${message.author}, This Channel For Media 🖼️ Only !`)
+    message.channel.send(`${message.author}, This channel for pics and vids only!`)
     .then(msg => {
-      setTimeout(() => {
+      setTimeout(() => {//Toxic Codes
         msg.delete();
-      }, 5000)
-  })
+      }, 5000)//Toxic Codes
+  })//Toxic Codes
   return;
-}
-   if(message.attachments.size >= 1) {
-    let filename = message.attachments.first().filename
-    console.log(filename);
-    if(!types.some( type => filename.endsWith(type) )) {
-      message.delete();
-      message.channel.send(`${message.author}, This Channel For Media 🖼️ Only !`)
-      .then(msg => {
-        setTimeout(() => {
-          msg.delete();
-        }, 5000)
-      })
-      .catch(err => {
-        console.error(err);
-    });
-    }
-  }
- })
-client.on('message', message => {
-  if(message.content.startsWith(prefix + "infoimage")) {
-let embed = new Discord.RichEmbed()
-.addField('Channel Status', `${pics[message.guild.id].onoff}`)
-.addField('Media Channel', `${pics[message.guild.id].channel}`)
-.addField('Requested By', `${message.author}`)
-.setThumbnail(message.author.avatarURL)
-.setFooter(`${client.user.username}`)
-message.channel.sendEmbed(embed)
-  }})
+} //Toxic Codes
 
 	
 	
